fbf35be0bd2b66c3dd8af1cf4a221dd64c17ba0a
ornoldoacebedo <ornoldoacebedo@gmail.com>
2023-02-28 10:34:25 -0300

    create api project vocabuilder

---
+++ src/main/java/com/example/vocabuilder/VocabuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.vocabuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabuilderApplication.class, args);
+    }
+
+}
